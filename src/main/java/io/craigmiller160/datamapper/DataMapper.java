4544e32cf7a6b9939e81692b068e8e9dc813880e
Craig Miller <craigmiller160@gmail.com>
2020-02-09 08:58:20 -0500

    Stubbed out more logic for DataMapper

--- src/main/java/io/craigmiller160/datamapper/DataMapper.java
+++ src/main/java/io/craigmiller160/datamapper/DataMapper.java
@@ -1,4 +1,21 @@
 package io.craigmiller160.datamapper;
 
+import io.craigmiller160.datamapper.config.MapperConfiguration;
+
 public class DataMapper {
+
+    private final MapperConfiguration configuration;
+
+    DataMapper(final MapperConfiguration configuration) {
+        this.configuration = configuration;
+    }
+
+    public <S,D> D mapToType(final S source, final Class<D> destination) {
+        throw new RuntimeException("Finish this");
+    }
+
+    public <S,D> void mapToInstance(final S source, final D destination) {
+        throw new RuntimeException("Finish this");
+    }
+
 }
--- src/main/java/io/craigmiller160/datamapper/factory/DataMapperFactory.java
+++
@@ -1,4 +0,0 @@
-package io.craigmiller160.datamapper.factory;
-
-public class DataMapperFactory {
-}
