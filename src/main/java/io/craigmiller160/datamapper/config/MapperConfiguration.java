feef3b2fda10b459348681d7f3b728c60f32fcad
Craig Miller <craigmiller160@gmail.com>
2020-02-09 08:55:51 -0500

    Built out some of the configuration settings

--- src/main/java/io/craigmiller160/datamapper/config/MapperConfiguration.java
+++ src/main/java/io/craigmiller160/datamapper/config/MapperConfiguration.java
@@ -1,4 +1,27 @@
 package io.craigmiller160.datamapper.config;
 
+import io.craigmiller160.datamapper.converter.Converter;
+
+import java.util.List;
+import java.util.Map;
+
 public class MapperConfiguration {
+
+    private final List<Converter> explicitConverters;
+    private final Map<Class<?>,List<String>> propertyIgnoreMap;
+
+    public MapperConfiguration(final List<Converter> explicitConverters,
+                               final Map<Class<?>,List<String>> propertyIgnoreMap) {
+        this.explicitConverters = explicitConverters;
+        this.propertyIgnoreMap = propertyIgnoreMap;
+    }
+
+    public List<Converter> getExplicitConverters() {
+        return explicitConverters;
+    }
+
+    public Map<Class<?>,List<String>> getPropertyIgnoreMap() {
+        return propertyIgnoreMap;
+    }
+
 }
