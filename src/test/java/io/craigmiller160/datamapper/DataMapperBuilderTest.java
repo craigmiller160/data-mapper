a1cd99a75c988bdcdf677e38d833e4eff0372494
Craig Miller <craigmiller160@gmail.com>
2020-02-09 10:13:56 -0500

    Stubbed out more tests

---
+++ src/test/java/io/craigmiller160/datamapper/DataMapperBuilderTest.java
@@ -0,0 +1,12 @@
+package io.craigmiller160.datamapper;
+
+import org.junit.Test;
+
+public class DataMapperBuilderTest {
+
+    @Test
+    public void test_writeTests() {
+        throw new RuntimeException();
+    }
+
+}
---
+++ src/test/java/io/craigmiller160/datamapper/converter/PrimitiveConverterTest.java
@@ -0,0 +1,12 @@
+package io.craigmiller160.datamapper.converter;
+
+import org.junit.Test;
+
+public class PrimitiveConverterTest {
+
+    @Test
+    public void test_writeTests() {
+        throw new RuntimeException();
+    }
+
+}
