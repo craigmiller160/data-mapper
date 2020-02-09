e94aad21d79b683a8e481b4a3b8aed82251af38c
Craig Miller <craigmiller160@gmail.com>
2020-02-09 10:02:21 -0500

    Finished the build() method of DataMapperBuilder

--- src/main/java/io/craigmiller160/datamapper/DataMapperBuilder.java
+++ src/main/java/io/craigmiller160/datamapper/DataMapperBuilder.java
@@ -1,5 +1,6 @@
 package io.craigmiller160.datamapper;
 
+import io.craigmiller160.datamapper.config.MapperConfiguration;
 import io.craigmiller160.datamapper.converter.Converter;
 
 import java.util.ArrayList;
@@ -13,7 +14,8 @@ public class DataMapperBuilder {
     private Map<Class<?>,List<String>> propertyIgnoreMap = new HashMap<>();
 
     public DataMapper build() {
-        throw new RuntimeException("Finish this");
+        final MapperConfiguration configuration = new MapperConfiguration(explicitConverters, propertyIgnoreMap);
+        return new DataMapper(configuration);
     }
 
     public DataMapperBuilder addExplicitConverter(final Converter<?,?> converter) {
