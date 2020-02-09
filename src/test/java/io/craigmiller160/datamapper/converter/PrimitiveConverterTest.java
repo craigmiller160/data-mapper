f54988c7176a8f127d4db84c5dd4b509f7108afc
Craig Miller <craigmiller160@gmail.com>
2020-02-09 10:30:02 -0500

    Stubbed out a LOT of tests for the PrimitiveConverter

--- src/test/java/io/craigmiller160/datamapper/converter/PrimitiveConverterTest.java
+++ src/test/java/io/craigmiller160/datamapper/converter/PrimitiveConverterTest.java
@@ -4,8 +4,255 @@ import org.junit.Test;
 
 public class PrimitiveConverterTest {
 
+    // TODO boolean
+
     @Test
-    public void test_writeTests() {
+    public void test_convert_byteToByte() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_stringToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convertStringToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_intToBoolean() {
+
+    }
+
+    @Test
+    public void test_convert_shortToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_shortToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_floatToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_doubleToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToLong() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_longToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToBoolean() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToString() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToInt() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToShort() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToFloat() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToDouble() {
+        throw new RuntimeException();
+    }
+
+    @Test
+    public void test_convert_booleanToLong() {
         throw new RuntimeException();
     }
 
