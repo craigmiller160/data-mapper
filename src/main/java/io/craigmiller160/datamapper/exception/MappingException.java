09033cdbc553cdf3b5ce4e2eaf93e5bc246d8248
Craig Miller <craigmiller160@gmail.com>
2020-02-09 09:53:49 -0500

    Implemented MappingException

--- src/main/java/io/craigmiller160/datamapper/exception/MappingException.java
+++ src/main/java/io/craigmiller160/datamapper/exception/MappingException.java
@@ -1,4 +1,20 @@
 package io.craigmiller160.datamapper.exception;
 
-public class MappingException {
+public class MappingException extends Exception {
+
+    public MappingException() {
+    }
+
+    public MappingException(String message) {
+        super(message);
+    }
+
+    public MappingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MappingException(Throwable cause) {
+        super(cause);
+    }
+
 }
